94de37183e3cdb0d63f62d95ede136afad81cf05
StevenW <StevenW.info@gmail.com>
2016-11-10 18:10:32 -0800

    Adding some information to cabal file and organizing things to make future development easier.

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,13 +8,22 @@ import qualified Network.HTTP.Simple as Http
 
 {-
 Notes:
-
 - Might want to make parts more complex than type synonyms.
 
 - Might want to provide a way to replace any non-string piece of the query with a string just to make it more flexible
 -   Also might want to be able to return it in several data types as well.
 -
 - Strings are the obvious type, Text is the more commonly recommended, and Bytestring is what http-conduit returns.
+-
+- Mention that I'd also potentially like to make a servant version, but that I would have to learn more about type-level programming.
+
+todo:
+- Create consumer API - in process
+- Create producer API
+- Add authentication and API token abilities.
+- (Look at other SODA libraries to get ideas)
+- Have it be able receive different files types using the right types.
+
  -}
 
 type Endpoint = String
@@ -25,15 +34,24 @@ type DatasetID = String
 -- Might want to make this a simpler type since the formats are currently limited.
 type ResponseFormat = String
 
-runRequest :: Request -> IO L8.ByteString
-runRequest request = do
+runRawRequest :: Request -> IO L8.ByteString
+runRawRequest request = do
     request <- Http.parseRequest request
     response <- Http.httpLBS request
     return $ Http.getResponseBody response
 
+runRequest :: Domain -> DatasetID -> ResponseFormat -> Query -> IO L8.ByteString
+runRequest domain datasetID format query= runRawRequest $ urlBuilder domain datasetID format query
+
+urlBuilder :: Domain -> DatasetID -> ResponseFormat -> Query -> Request
+urlBuilder domain datasetID format query = "https://" ++ domain ++ "/resource/" ++ datasetID ++ format ++ query
+
+testRequest :: Request
+testRequest = "https://" ++ testDomain ++ "/resource/" ++ testDataset ++ testResponse ++ testQuery
+
 someFunc :: IO ()
 someFunc = do
-    response <- runRequest testRequest
+    response <- runRequest testDomain testDataset testResponse testQuery
     L8.putStrLn response
     -- I'll probably need to use the status code and response header later
     --putStrLn $ "The status code was: " ++
@@ -41,6 +59,7 @@ someFunc = do
     --print $ Http.getResponseHeader "Content-Type" response
 
 -- Test stuff
+-- I know I should just make actual tests, but I'm new to this so I'll do that later.
 
 testDomain :: Domain
 testDomain = "open.whitehouse.gov"
@@ -53,6 +72,3 @@ testResponse = ".json"
 
 testQuery :: Query
 testQuery = "?name=Drain,%20Gershwin%20A."
-
-testRequest :: Request
-testRequest = "https://" ++ testDomain ++ "/resource/" ++ testDataset ++ testResponse ++ testQuery
