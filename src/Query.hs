265a9871fed1a4b7f644fb5c3054590ef1f29757
StevenW <StevenW.info@gmail.com>
2016-11-17 21:31:59 -0800

    A few small improvements and baby steps forward in the model.

--- src/Query.hs
+++ src/Query.hs
@@ -3,6 +3,10 @@ module Query
     ( 
     ) where
 
+{-|
+Elements which make SoQL and other parts of identifying what data you want easier.
+ -}
+
 {- Notes:
 
 - Need to create smart constructors.
@@ -10,6 +14,12 @@ module Query
 - I'll have to think through how subqueries will be done. Might be a little complicated.
 -
 - The logic seems very similar to a language. Abstract syntax tree, etc. I suppose that seems kind of obvious because it's the Socrata Query *Language* which is based on SQL, another language.
+
+query -> queryinfo
+
+TODO:
+- Run hlint and see how much is unnecessary.
+- Figure out better names.
 
  -}
 
@@ -41,6 +51,9 @@ type Column = String
 -- Could change content in the future because we can enforce it to follow the given datatypes
 type Content = String
 
+-- |Placeholder for more complex type later
+type Predicate = String
+
 -- Obviously completely untrue, but I'll use it to keep track of where I want it to be true.
 type NonNegative = Int
 
@@ -51,9 +64,11 @@ data Sorting = ASC | DESC
 
 -- Possibly be more specific in the types like "Column" or something.
 -- Need to account for negative limit, which doesn't make sense, somehow.
-data Clause = Filter Column Content
+-- Don't export limit, offset, or Combine.
+-- Is putting the combine there going to make it more difficult to decompose queries?
+data Query = Filter Column Content
     | Select Column 
-    | Where String 
+    | Where Predicate 
     | Order Column (Maybe Sorting)
     | Group String 
     | Having String 
@@ -62,30 +77,56 @@ data Clause = Filter Column Content
     | Q String 
     | SubQuery Query 
     | Bom Bool
+    | Combine Query Query
 
-limit :: Int -> Maybe Clause
+limit :: Int -> Maybe Query
 limit int
     | int < 0 = Nothing
     | otherwise = Just (Limit int)
 
-offset :: Int -> Maybe Clause
+offset :: Int -> Maybe Query
 offset int
     | int < 0 = Nothing
     | otherwise = Just (Offset int)
 
--- Could maybe turn Clause into Query and then have a constructor which is recursive?
-newtype Query = Query { getClauses :: [Clause] }
+-- This actually can be a monoid.
+-- |I'd like to have most of this info at the type level, but I would need to learn more type level programming in Haskell to implement that.
+data QueryMeta = QueryMeta { whereExists :: Bool
+                           }
 
--- Just don't export this one I guess.
-addClause :: Clause -> Query -> Query
-addClause clause (Query clauses) = Query (clause : clauses)
+-- |Need a better name. Also don't export the constructor.
+-- |Also, maybe have a way to have "default values" since I'm usually only changing one thing at a time.
+-- This feels so much like a monad, yet I'm not exactly sure how to make the initial context depend on the value. I don't want a return to violate the dependency. Maybe the solution is to bring the metadata up to the type level.
+-- |Just makes finding errors more efficient. Sort of like memoization or caching.
+data QueryInfo = QueryInfo Query QueryMeta
 
-data QueryError = MultipleWith
+defaultQueryInfo = { whereExists = False }
 
--- Turn this into monadplus or monoid or whatever in the future.
--- addQuery :: Clause -> Query -> Either QueryError Query
+queryInfo :: Query -> QueryInfo
+queryInfo (Query Where pred) = QueryInfo (Query Where pred) (QueryMeta (defaultQueryInfo { whereExists = True }))
+queryInfo query = QueryInfo Query QueryMeta
+
+-- Need to figure out better naming
+combineQ :: QueryInfo -> QueryInfo -> Either QueryError QueryInfo
+combineQ (QueryInfo q1 qi1) (QueryInfo q2 qi2) = case combineQI qi1 qi2 of
+                                                     Right qi -> Right QueryInfo (Combine q1 q2) qi
+                                                     Left qe -> Left qe
+
+-- There has got to be a simpler way of writing this (that's still extensible).
+combineQI :: QueryMeta -> QueryMeta -> Either QueryError QueryMeta
+combineQI qm1 qm2 = { whereExists = ( whereCheck qm1 qm2 ) }
+    where whereCheck { whereExists = we1 } { whereExists = we2 } = we1 && we2
+
+-- Reader or writer monad to capture implementation information.
+-- WriterT QueryMeta Either QueryError Query
+-- Although, maybe not. The problem is creating the WriterT with the restrictions on QueryMeta.
+
+-- This might be a monoid? Also, make a show instance for it.
+-- |Should match up with QueryMeta since QueryMeta just makes finding errors more efficient.
+data QueryError = { numberWhere :: Int }
 
 -- SoSQL functions
---
+-- TODO
+
 -- Paging functionality
---
+-- TODO
