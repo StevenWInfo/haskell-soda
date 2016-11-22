b670f99eabfbf226e00921576f95f74a34b350cc
StevenW <StevenW.info@gmail.com>
2016-11-22 12:12:44 -0800

    After reviewing the SODA specifications, I am simplifying how this works by quite a bit. Query is a record type now.

--- src/Query.hs
+++ src/Query.hs
@@ -21,13 +21,12 @@ Elements which make SoQL and other parts of identifying what data you want easie
 
 - I need to forget about abstracting it into space and just get something workable done. Then I can worry about abstracting things out.
 
-query -> queryinfo
-
 - The "these" package would allow me to give more information about errors, but might be more inefficient because it wouldn't short-circuit computation.
 
 TODO:
 - Run hlint and see how much is unnecessary.
 - Figure out better names.
+- Clean up and make things like naming more consistant.
 
  -}
 
@@ -72,8 +71,23 @@ data Sorting = ASC | DESC
 
 -- Possibly be more specific in the types like "Column" or something.
 -- Need to account for negative limit, which doesn't make sense, somehow.
--- Don't export limit, offset, or Combine.
--- Is putting the combine there going to make it more difficult to decompose queries?
+-- Don't export constructor
+-- Either have maybes for all of these or have an empty indicator for all types.
+-- Custom datatypes for some of these
+data Query = Query { filter :: [String] -- Type with columns and contents
+                   , select :: [String]
+                   , whereC :: String -- Is the lowercase where allowed?
+                   , order :: String
+                   , group :: String
+                   , having :: String -- Expand later?
+                   , limit :: NonNegative
+                   , offset :: NonNegative
+                   , search :: String -- |$q parameter
+                   , subquery :: Query
+                   , bom :: Bool
+                   }
+
+{- Going to get rid of but keeping it around to get useful stuff out of it.
 data Query = Filter Column Content
     | Select Column 
     | Where Predicate 
@@ -86,7 +100,9 @@ data Query = Filter Column Content
     | SubQuery Query -- |$query parameter
     | Bom Bool
     | Combine Query Query
+-}
 
+{-
 limit :: Int -> Maybe Query
 limit int
     | int < 0 = Nothing
@@ -96,24 +112,13 @@ offset :: Int -> Maybe Query
 offset int
     | int < 0 = Nothing
     | otherwise = Just (Offset int)
+-}
 
--- This actually can be a monoid.
--- |I'd like to have most of this info at the type level, but I would need to learn more type level programming in Haskell to implement that.
-data QueryMeta = QueryMeta { whereExists :: Bool
-                           }
+-- Do I still want this?
+-- data QueryMeta = QueryMeta { whereExists :: Bool
+-- data QueryInfo = QueryInfo Query QueryMeta
 
--- |Need a better name. Also don't export the constructor.
--- |Also, maybe have a way to have "default values" since I'm usually only changing one thing at a time.
--- This feels so much like a monad, yet I'm not exactly sure how to make the initial context depend on the value. I don't want a return to violate the dependency. Maybe the solution is to bring the metadata up to the type level. (Maybe the Bind typeclass I stumbled across).
--- |Just makes finding errors more efficient. Sort of like memoization or caching.
-data QueryInfo = QueryInfo Query QueryMeta
-
-defaultQueryMeta = QueryMeta { whereExists = False }
-
-queryInfo :: Query -> QueryInfo
-queryInfo (Where pred) = QueryInfo (Where pred) (defaultQueryMeta { whereExists = True })
-queryInfo query = QueryInfo query defaultQueryMeta
-
+{-
 -- Need to figure out better naming
 combineQ :: QueryInfo -> QueryInfo -> Either QueryError QueryInfo
 combineQ (QueryInfo q1 qi1) (QueryInfo q2 qi2) = case combineQI qi1 qi2 of
@@ -126,15 +131,11 @@ combineQI :: QueryMeta -> QueryMeta -> Either QueryError QueryMeta
 combineQI qm1 qm2 = do
     isWhere <- whereCheck qm1 qm2
     return (QueryMeta { whereExists = isWhere })
-
-whereCheck :: QueryMeta -> QueryMeta -> Either QueryError Bool
-whereCheck (QueryMeta { whereExists = we1 }) (QueryMeta { whereExists = we2 })
-    | we1 && we2 = Left MultipleWhere
-    | otherwise  = Right (we1 || we2)
+-}
 
 -- This might be a monoid? Also, make a show instance for it.
--- |Should match up with QueryMeta since QueryMeta just makes finding errors more efficient.
-data QueryError = MultipleWhere
+-- Need to figure out how this works.
+data QueryError = WhereError
 
 -- SoSQL functions
 -- TODO
