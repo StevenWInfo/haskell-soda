010da935c5f0c7044a6a711bb89c6175e291c90a
StevenW <StevenW.info@gmail.com>
2016-12-04 20:19:01 -0800

    Preparing test stuff.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
+
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+-- main = runTestTT tests
+
+-- tests = ...
