34c7b09647bed47234894c26079f69391eafb80b
StevenW <StevenW.info@gmail.com>
2016-12-11 15:15:02 -0800

    Some leftover stuff from trying out the JSON stuff and trying out the exception handling.

--- src/Soda.hs
+++ src/Soda.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Soda
     ( ResponseFormat (..)
     , formatToUrl
@@ -6,35 +7,34 @@ module Soda
     , DatasetID
     , RawParameters
     , urlBuilder
-    , runRequest
+    , getStringResponse
+    , getStringBody
+    --, getJsonResponse
     ) where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import System.IO
 import Data.Text (Text, pack, append)
-import Data.Monoid ((<>))
+import Data.Monoid ((<>), Monoid)
+--import GHC.Generics
+--import Data.Aeson
 import Network.HTTP.Req
 import Control.Exception
 
 import Query
 
-{-
-#Notes
+{- #Notes
 
 - Might want to make parts more complex than type synonyms.
 
 - Might want to provide a way to replace any non-string piece of the query with a string just to make it more flexible
     - Also might want to be able to return it in several data types as well.
 
-- Strings are the obvious type, Text is the more commonly recommended, and Bytestring is what http-conduit returns.
-
 - Mention that I'd also potentially like to make a servant version, but that I would have to learn more about type-level programming.
 
 - Have ways to change the return string format as well as whether it is given as a string type or a more native format.
     - Should it just give it as a string and the user parses it into a native type/format or should I include things to automatically convert it?
     - Maybe I'll put it in eventually, but for now just return as string.
-
-- Perhaps, try out that new REST library.
 
 ##todo
 
@@ -48,38 +48,52 @@ import Query
 - Handle error responses
 - Have some way to run raw queries and turn complete or partial EDSL queries into raw queries.
 - Not sure if I want to use String, Text, Bytestring, etc. Decide eventually.
-
+- Figure out how to get info out in JSON, XML, and other types. JSON was a little more complicated than I had expected.
  -}
 
-type Endpoint = String
-type Request = String
-type RawParameters = [(Text, String)]
-type Domain = String
-type DatasetID = String
+type Endpoint      = String
+type Request       = String
+type RawParameters = [(String, String)]
+type Domain        = String
+type DatasetID     = String
 
 -- Have this affect the mime type?
 data ResponseFormat = CSV | GeoJSON | JSON | RDFXML | XML
 
--- Should it maybe put the dot in front? Perhaps make it part of a typeclass instead?
+-- Should it maybe put the dot in front? Perhaps make it part of Show?
 formatToUrl :: ResponseFormat -> String
-formatToUrl CSV = "csv"
+formatToUrl CSV     = "csv"
 formatToUrl GeoJSON = "geojson"
-formatToUrl JSON = "json"
-formatToUrl RDFXML = "rdf"
-formatToUrl XML = "xml"
+formatToUrl JSON    = "json"
+formatToUrl RDFXML  = "rdf"
+formatToUrl XML     = "xml"
 
 instance MonadHttp IO where
   handleHttpException = throwIO
 
-runRequest :: Domain -> DatasetID -> ResponseFormat -> RawParameters -> IO String
-runRequest domain datasetID format query = do
+-- Misnamed right now because the output is an LbsResponse
+-- Maybe should accept a request record type.
+--getStringResponse :: Domain -> DatasetID -> ResponseFormat -> RawParameters -> IO String
+getStringResponse domain datasetID format query = do
     let url = urlBuilder domain datasetID format
-    let param = foldr1 (<>) $ map (\(x,y) -> x =: y) query
+    let param = foldr1 (<>) $ map (\(x,y) -> (pack x) =: y) query
     response <- req GET url NoReqBody lbsResponse param
-    return (L8.unpack (responseBody response))
+    return response
 
--- Eventually change RawParameters to Query type
---urlBuilder :: Domain -> DatasetID -> ResponseFormat -> RawParameters -> Url Https
+getStringBody :: Domain -> DatasetID -> ResponseFormat -> RawParameters -> IO String
+getStringBody domain datasetID format query = (getStringResponse domain datasetID format query) >>= (return . L8.unpack . responseBody)
+
+{-
+-- Perhaps figuring out how to return different types based on other parameters would be better.
+--getJsonResponse :: Domain -> DatasetID -> ResponseFormat -> RawParameters -> IO a
+getJsonResponse domain datasetID format query = do
+    response <- req GET url NoReqBody jsonResponse param
+    return $ responseBody response
+    where url   = urlBuilder domain datasetID format
+          param = foldr1 (<>) $ map (\(x,y) -> (pack x) =: y) query
+          -}
+
+--urlBuilder :: Domain -> DatasetID -> ResponseFormat -> Url Https
 urlBuilder domain datasetID format = https domain' /: "resource" /: (datasetID' `append` "." `append` format')
     where domain' = pack domain
           datasetID' = pack datasetID
--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -18,6 +18,8 @@ import Control.Exception
 
 main :: IO ()
 main = do
+    response <- getStringResponse (tail testDomain) testDataset testFormat testQuery
+    --putStrLn . show $ responseStatusCode response
     defaultMain tests
 
 tests = testGroup "Tests" [unitTests]
@@ -26,10 +28,16 @@ tests = testGroup "Tests" [unitTests]
 unitTests = testGroup "Unit tests"
     [ testCase "Try out executing query." $ do
         contents <- readFile "tests/data/4tka-6guvMag3.json"
-        response <- runRequest testDomain testDataset testFormat testQuery
-        response @?= (init contents) -- I haven't tested this yet.
-    --, testCase "urlBuilder smoke test" $
-        --urlBuilder testDomain testDataset testFormat testQuery @?= "https://soda.demo.socrata.com/resource/4tka-6guv.json?$where=magnitude > 3.0"
+        response <- getStringBody testDomain testDataset testFormat testQuery
+        response @?= (init contents)
+    , testCase "Test 404 response" $ do
+        response <- try (getStringResponse (tail testDomain) testDataset testFormat testQuery) :: IO (Either HttpException LbsResponse)
+        case response of
+            Left ex -> do 
+                          True @? "Threw exception"
+                          --(responseStatusCode ex) @?= 404
+            Right foo -> do
+                            False @? "Should have thrown exception"
     ]
 
 {-
@@ -50,3 +58,39 @@ testFormat = JSON
 
 testQuery :: RawParameters
 testQuery = [("$where", "magnitude > 3.0")]
+
+{-
+data Earthquake =
+    Earthquake { source :: String
+               , earthquake_id :: String
+               , version :: String
+               , magnitude :: Double
+               , depth :: Double
+               , number_of_stations :: Double
+               , region :: String
+               , location_city :: String
+               , location :: String
+               , location_address :: String
+               , location_zip :: String
+               , location_state :: String
+               } deriving (Show, Generic)
+
+--instance FromJSON Earthquake
+instance FromJSON Earthquake where
+    parseJSON (Object v) =
+        Earthquake <$> v .:? "source" .!= ""
+                   <*> v .:? "earthquake_id" .!= ""
+                   <*> v .:? "version" .!= ""
+                   <*> v .:? "magnitude" .!= ""
+                   <*> v .:? "depth" .!= ""
+                   <*> v .:? "number_of_stations" .!= ""
+                   <*> v .:? "region" .!= ""
+                   <*> v .:? "location_city" .!= ""
+                   <*> v .:? "location" .!= ""
+                   <*> v .:? "location_address" .!= ""
+                   <*> v .:? "location_zip" .!= ""
+                   <*> v .:? "location_state" .!= ""
+    parseJSON _ = mzero
+
+instance ToJSON Earthquake
+-}
